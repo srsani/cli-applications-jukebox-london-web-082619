ea4fd642f556f61bc3c7b29a3db7ae7ae3fc5719
droll-accelerator-1483 <srsani@gmail.com>
2019-07-29 06:44:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,16 +1,16 @@
 # Add your code here
 
-# songs = [
-#   "Phoenix - 1901",
-#   "Tokyo Police Club - Wait Up",
-#   "Sufjan Stevens - Too Much",
-#   "The Naked and the Famous - Young Blood",
-#   "(Far From) Home - Tiga",
-#   "The Cults - Abducted",
-#   "Phoenix - Consolation Prizes",
-#   "Harry Chapin - Cats in the Cradle",
-#   "Amos Lee - Keep It Loose, Keep It Tight"
-# ]
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
 
 def help ()
   puts 'I accept the following commands:'
@@ -68,9 +68,9 @@ def run()
     user_input = gets.chomp
     if user_input == "help"
       help()
-    if user_input == "play"
+    elsif user_input == "play"
       play(songs)
-    if user_input == "list"
+    elsif user_input == "list"
       list(songs)
     elsif user_input == "exit"
       exit_jukebox
